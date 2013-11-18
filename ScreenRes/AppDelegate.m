20f305f351f94451e3e443e1340c527442419499
Colin McDonald <colinmcdonald@gmail.com>
2013-11-18 12:50:49 -0500

    Cleanup code (mostly dead un-needed CoreData related stuff).

--- ScreenRes/AppDelegate.m
+++ ScreenRes/AppDelegate.m
@@ -1,15 +1,8 @@
-//
-//  AppDelegate.m
-//  ScreenRes
-//
-//  Created by Colin McDonald on 2013-10-07.
-//  Copyright (c) 2013 Colin McDonald. All rights reserved.
-//
-
 #import "AppDelegate.h"
 #import "cg_utils.h"
 
 @interface AppDelegate()
+    @property (assign) IBOutlet NSMenu *statusMenu;
     @property (nonatomic, strong) NSMutableArray *currentDisplayMode;
     @property (nonatomic, strong) NSMutableArray *displayModesArray;
     @property (nonatomic, strong) NSMutableArray *menuItemsArray;
@@ -18,27 +11,31 @@
 
 @implementation AppDelegate
 
-@synthesize persistentStoreCoordinator = _persistentStoreCoordinator;
-@synthesize managedObjectModel = _managedObjectModel;
-@synthesize managedObjectContext = _managedObjectContext;
 @synthesize statusMenu, displayModesArray, menuItemsArray, statusItem, currentDisplayMode;
 
-
-- (void)applicationDidFinishLaunching:(NSNotification *)aNotification
-{
-    // Insert code here to initialize your application
+- (void)applicationDidFinishLaunching:(NSNotification *)aNotification {
+    // stub
 }
 
--(void)awakeFromNib
-{
+-(void)awakeFromNib {
+    // when app first starts, populate available display modes
+    // in status bar menu for each active display
     [self configureStatusBar];
 }
 
+-(void) applicationDidChangeScreenParameters:(NSNotification *)notification {
+    // when screen resolutions change, monitors are plugged-in or unplugged etc,
+    // this  method will get fired to re-populate the display options menu
+    [self configureStatusBar];
+}
+
+-(IBAction) quitApp:(id) sender {
+    // terminate application at user's request...
+    [[NSApplication sharedApplication] terminate:nil];
+}
+
 -(void) configureStatusBar {
-    self.displayModesArray = [self populateDisplayModesArray];
-    self.menuItemsArray = [self populateStatusBarMenuOptionsFromDisplayArray:self.displayModesArray];
-    [self configureStatusMenu:self.statusMenu WithOptions:self.menuItemsArray];
-    
+    // set status bar icon properties
     if(nil == self.statusItem) {
         self.statusItem = [[NSStatusBar systemStatusBar] statusItemWithLength:NSSquareStatusItemLength];
         [self.statusItem setImage:[NSImage imageNamed:@"Icon.png"]];
@@ -46,25 +43,29 @@
         [self.statusItem setHighlightMode:YES];
     }
     
+    // get supported display modes for each display, save in array
+    self.displayModesArray = [self populateDisplayModesArray];
+    
+    // populate menu items from array of supported display modes
+    self.menuItemsArray = [self populateStatusBarMenuOptionsFromDisplayArray:self.displayModesArray];
+    [self configureStatusMenu:self.statusMenu WithOptions:self.menuItemsArray];
     [self.statusItem setMenu:self.statusMenu];
 }
 
--(void) applicationDidChangeScreenParameters:(NSNotification *)notification {
-    [self configureStatusBar];
-}
-
 -(void) configureStatusMenu:(NSMenu *)menu WithOptions:(NSArray *) menuItems {
+    // clear existing menu options before populating
     [menu removeAllItems];
     
+    // create submenu for each active display
     for(int i=0; i < menuItems.count; i++)
     {
+        // add item in main status bar menu for display
         NSString *subMenuTitle = [NSString stringWithFormat:@"Display %d", (i + 1)];
         NSMenuItem *displayButton = [[NSMenuItem alloc] initWithTitle:subMenuTitle action:nil keyEquivalent:@""];
-        
         [menu addItem:displayButton];
         
+        // populate display submenu with it's supported display modes
         NSMenu *displayMenu = [[NSMenu alloc] init];
-        
         for(int j=0; j < ((NSArray *)menuItems[i]).count; j++) {
             [displayMenu addItem:menuItems[i][j]];
         }
@@ -72,6 +73,7 @@
         [menu setSubmenu:displayMenu forItem:displayButton];
     }
     
+    // add the 'Quit' option to main application menu
     [menu addItem:[NSMenuItem separatorItem]];
     NSMenuItem *quitItem = [[NSMenuItem alloc] initWithTitle:@"Quit" action:@selector(quitApp:) keyEquivalent:@""];
     [menu addItem:quitItem];
@@ -185,14 +187,6 @@
     return menuItems;
 }
 
--(NSString *) stringFromDisplayMode:(CGDisplayModeRef) mode {
-    return [NSString stringWithFormat:@"%zu x %zu @ %.0fhz (%zu bit)",
-                                 CGDisplayModeGetWidth(mode),
-                                 CGDisplayModeGetHeight(mode),
-                                 CGDisplayModeGetRefreshRate(mode),
-                                 bitDepth(mode)];
-}
-
 -(IBAction) displayOptionWasSelected:(id) sender {
     for(int idxDisplay = 0; idxDisplay < menuItemsArray.count; idxDisplay++)
     {
@@ -233,169 +227,13 @@
     }
 }
 
--(IBAction) quitApp:(id) sender {
-    [[NSApplication sharedApplication] terminate:nil];
-}
-
-// Returns the directory the application uses to store the Core Data store file. This code uses a directory named "m.com.colin-mcdonald.ScreenRes" in the user's Application Support directory.
-- (NSURL *)applicationFilesDirectory
-{
-    NSFileManager *fileManager = [NSFileManager defaultManager];
-    NSURL *appSupportURL = [[fileManager URLsForDirectory:NSApplicationSupportDirectory inDomains:NSUserDomainMask] lastObject];
-    return [appSupportURL URLByAppendingPathComponent:@"m.com.colin-mcdonald.ScreenRes"];
-}
-
-// Creates if necessary and returns the managed object model for the application.
-- (NSManagedObjectModel *)managedObjectModel
-{
-    if (_managedObjectModel) {
-        return _managedObjectModel;
-    }
-	
-    NSURL *modelURL = [[NSBundle mainBundle] URLForResource:@"ScreenRes" withExtension:@"momd"];
-    _managedObjectModel = [[NSManagedObjectModel alloc] initWithContentsOfURL:modelURL];
-    return _managedObjectModel;
-}
-
-// Returns the persistent store coordinator for the application. This implementation creates and return a coordinator, having added the store for the application to it. (The directory for the store is created, if necessary.)
-- (NSPersistentStoreCoordinator *)persistentStoreCoordinator
-{
-    if (_persistentStoreCoordinator) {
-        return _persistentStoreCoordinator;
-    }
-    
-    NSManagedObjectModel *mom = [self managedObjectModel];
-    if (!mom) {
-        NSLog(@"%@:%@ No model to generate a store from", [self class], NSStringFromSelector(_cmd));
-        return nil;
-    }
-    
-    NSFileManager *fileManager = [NSFileManager defaultManager];
-    NSURL *applicationFilesDirectory = [self applicationFilesDirectory];
-    NSError *error = nil;
-    
-    NSDictionary *properties = [applicationFilesDirectory resourceValuesForKeys:@[NSURLIsDirectoryKey] error:&error];
-    
-    if (!properties) {
-        BOOL ok = NO;
-        if ([error code] == NSFileReadNoSuchFileError) {
-            ok = [fileManager createDirectoryAtPath:[applicationFilesDirectory path] withIntermediateDirectories:YES attributes:nil error:&error];
-        }
-        if (!ok) {
-            [[NSApplication sharedApplication] presentError:error];
-            return nil;
-        }
-    } else {
-        if (![properties[NSURLIsDirectoryKey] boolValue]) {
-            // Customize and localize this error.
-            NSString *failureDescription = [NSString stringWithFormat:@"Expected a folder to store application data, found a file (%@).", [applicationFilesDirectory path]];
-            
-            NSMutableDictionary *dict = [NSMutableDictionary dictionary];
-            [dict setValue:failureDescription forKey:NSLocalizedDescriptionKey];
-            error = [NSError errorWithDomain:@"YOUR_ERROR_DOMAIN" code:101 userInfo:dict];
-            
-            [[NSApplication sharedApplication] presentError:error];
-            return nil;
-        }
-    }
-    
-    NSURL *url = [applicationFilesDirectory URLByAppendingPathComponent:@"ScreenRes.storedata"];
-    NSPersistentStoreCoordinator *coordinator = [[NSPersistentStoreCoordinator alloc] initWithManagedObjectModel:mom];
-    if (![coordinator addPersistentStoreWithType:NSXMLStoreType configuration:nil URL:url options:nil error:&error]) {
-        [[NSApplication sharedApplication] presentError:error];
-        return nil;
-    }
-    _persistentStoreCoordinator = coordinator;
-    
-    return _persistentStoreCoordinator;
-}
-
-// Returns the managed object context for the application (which is already bound to the persistent store coordinator for the application.) 
-- (NSManagedObjectContext *)managedObjectContext
-{
-    if (_managedObjectContext) {
-        return _managedObjectContext;
-    }
-    
-    NSPersistentStoreCoordinator *coordinator = [self persistentStoreCoordinator];
-    if (!coordinator) {
-        NSMutableDictionary *dict = [NSMutableDictionary dictionary];
-        [dict setValue:@"Failed to initialize the store" forKey:NSLocalizedDescriptionKey];
-        [dict setValue:@"There was an error building up the data file." forKey:NSLocalizedFailureReasonErrorKey];
-        NSError *error = [NSError errorWithDomain:@"YOUR_ERROR_DOMAIN" code:9999 userInfo:dict];
-        [[NSApplication sharedApplication] presentError:error];
-        return nil;
-    }
-    _managedObjectContext = [[NSManagedObjectContext alloc] init];
-    [_managedObjectContext setPersistentStoreCoordinator:coordinator];
-
-    return _managedObjectContext;
-}
-
-// Returns the NSUndoManager for the application. In this case, the manager returned is that of the managed object context for the application.
-- (NSUndoManager *)windowWillReturnUndoManager:(NSWindow *)window
-{
-    return [[self managedObjectContext] undoManager];
-}
-
-// Performs the save action for the application, which is to send the save: message to the application's managed object context. Any encountered errors are presented to the user.
-- (IBAction)saveAction:(id)sender
-{
-    NSError *error = nil;
-    
-    if (![[self managedObjectContext] commitEditing]) {
-        NSLog(@"%@:%@ unable to commit editing before saving", [self class], NSStringFromSelector(_cmd));
-    }
-    
-    if (![[self managedObjectContext] save:&error]) {
-        [[NSApplication sharedApplication] presentError:error];
-    }
-}
-
-- (NSApplicationTerminateReply)applicationShouldTerminate:(NSApplication *)sender
-{
-    // Save changes in the application's managed object context before the application terminates.
-    
-    if (!_managedObjectContext) {
-        return NSTerminateNow;
-    }
-    
-    if (![[self managedObjectContext] commitEditing]) {
-        NSLog(@"%@:%@ unable to commit editing to terminate", [self class], NSStringFromSelector(_cmd));
-        return NSTerminateCancel;
-    }
-    
-    if (![[self managedObjectContext] hasChanges]) {
-        return NSTerminateNow;
-    }
-    
-    NSError *error = nil;
-    if (![[self managedObjectContext] save:&error]) {
-
-        // Customize this code block to include application-specific recovery steps.              
-        BOOL result = [sender presentError:error];
-        if (result) {
-            return NSTerminateCancel;
-        }
-
-        NSString *question = NSLocalizedString(@"Could not save changes while quitting. Quit anyway?", @"Quit without saves error question message");
-        NSString *info = NSLocalizedString(@"Quitting now will lose any changes you have made since the last successful save", @"Quit without saves error question info");
-        NSString *quitButton = NSLocalizedString(@"Quit anyway", @"Quit anyway button title");
-        NSString *cancelButton = NSLocalizedString(@"Cancel", @"Cancel button title");
-        NSAlert *alert = [[NSAlert alloc] init];
-        [alert setMessageText:question];
-        [alert setInformativeText:info];
-        [alert addButtonWithTitle:quitButton];
-        [alert addButtonWithTitle:cancelButton];
-
-        NSInteger answer = [alert runModal];
-        
-        if (answer == NSAlertAlternateReturn) {
-            return NSTerminateCancel;
-        }
-    }
-
-    return NSTerminateNow;
+-(NSString *) stringFromDisplayMode:(CGDisplayModeRef) mode {
+    // return formatted string representation of a given display mode
+    return [NSString stringWithFormat:@"%zu x %zu @ %.0fhz (%zu bit)",
+            CGDisplayModeGetWidth(mode),
+            CGDisplayModeGetHeight(mode),
+            CGDisplayModeGetRefreshRate(mode),
+            bitDepth(mode)];
 }
 
 @end
--- ScreenRes/main.m
+++ ScreenRes/main.m
@@ -1,14 +1,5 @@
-//
-//  main.m
-//  ScreenRes
-//
-//  Created by Colin McDonald on 2013-10-07.
-//  Copyright (c) 2013 Colin McDonald. All rights reserved.
-//
-
 #import <Cocoa/Cocoa.h>
 
-int main(int argc, const char * argv[])
-{
+int main(int argc, const char * argv[]) {
     return NSApplicationMain(argc, argv);
 }
